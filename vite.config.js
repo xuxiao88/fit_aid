faf04b4963d0eef394a6e5a14a25adee6841bf26
xuxiao88 <xuxiao.nanjing@gmail.com>
2025-08-12 10:34:34 +0800

    chore: auto commit before deploy

--- vite.config.js
+++ vite.config.js
@@ -10,7 +10,7 @@ export default defineConfig({
     vue(),
     vueDevTools(),
   ],
-  base: '/fit_aid/',
+  // base: '/fit_aid/',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
